552502d738a0f663ee5fc8f73f3b6094db45a5ce
Idan Ohayon <iidan20@gmail.com>
2021-10-31 12:19:47 +0200

    Update ProductRepository.java

--- src/main/java/net/ChangeWithMe/SpringBootWebApp/Repository/ProductRepository.java
+++ src/main/java/net/ChangeWithMe/SpringBootWebApp/Repository/ProductRepository.java
@@ -12,30 +12,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "    campaign c\n" +
             "        INNER JOIN\n" +
             "    product p ON c.id = p.campaign_id\n" +
-            "        AND p.category= :category\n" +
             "        AND c.start_date >= (NOW() - INTERVAL 10 DAY)\n" +
-            "WHERE\n" +
-            "    c.id IN (SELECT \n" +
-            "            id\n" +
-            "        FROM\n" +
-            "            campaign)\n" +
-            "ORDER BY c.bid DESC\n" +
+            "ORDER BY p.category= :category, c.bid DESC\n" +
             "LIMIT 1;", nativeQuery = true)
     Product getProductWithHighestBidByCategory(@Param("category") String category);
-
-    @Query(value = "SELECT \n" +
-            "    p.*\n" +
-            "FROM\n" +
-            "    campaign c\n" +
-            "        INNER JOIN\n" +
-            "    product p ON c.id = p.campaign_id\n" +
-            "WHERE\n" +
-            "    c.bid IN (SELECT \n" +
-            "            MAX(bid)\n" +
-            "        FROM\n" +
-            "            campaign\n" +
-            "        WHERE\n" +
-            "            start_date >= (NOW() - INTERVAL 10 DAY))\n" +
-            "LIMIT 1;", nativeQuery = true)
-    Product getPromotedProductWithHighestBid();
 }
